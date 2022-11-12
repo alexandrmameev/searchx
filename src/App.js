6ad3b31fb9ac57737a57d02af64a22c8219aa616
Alex M <Alexandrdrd@gmail.com>
2022-11-12 16:41:37 +0100

    Change CSS to SCSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Search from './pages/Search';
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
