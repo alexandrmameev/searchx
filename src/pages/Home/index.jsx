b631a49e340d5ea219826036ab410264bca27593
Alex M <Alexandrdrd@gmail.com>
2022-11-12 20:55:48 +0100

    Components: Logo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,21 +1,14 @@
-import { Link } from 'react-router-dom';
-
 import SearchField from '../../components/SearchField';
+import Logo from '../../components/Logo';
 
 import './index.scss';
 
 const Home = () => {
   return (
     <div className="home__page">
-      <Link to="/" className="home__logo">
-        <span className="blue">S</span>
-        <span className="red">e</span>
-        <span className="yellow">a</span>
-        <span className="yellow">r</span>
-        <span className="blue">c</span>
-        <span className="green">h</span>
-        <span className="red">X</span>
-      </Link>
+      <div className="home__logo">
+        <Logo />
+      </div>
       <div className="home__search">
         <SearchField autoFocus={true} />
       </div>
